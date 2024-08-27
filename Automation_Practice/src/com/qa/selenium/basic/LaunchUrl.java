77544d9a7991ac290073173918b331dfa4172145
dkumarauto93 <dinesh.kashyap1978@hotmail.com>
2024-08-27 20:45:15 +0530

    Launch URL

--- Automation_Practice/src/com/qa/selenium/basic/LaunchUrl.java
+++ Automation_Practice/src/com/qa/selenium/basic/LaunchUrl.java
@@ -1,7 +1,17 @@
 package com.qa.selenium.basic;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LaunchUrl {
+    static String appURL = "https://artoftesting.com/samplesiteforselenium";
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(appURL);
 
 
-
+    }
 }
